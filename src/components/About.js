56ee937222a0198a339579c42169e0d3181052fa
son loku <jacobrborgen@gmail.com>
2020-08-02 23:00:42 -0500

    added contact info to contact page

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import Wrapper from './Wrapper'
 
 function About() {
   return (
     <div className="aboutPage" id='about'>
-     <div>
-       <img alt='selfie' src='../../images/selfie.png' />
-     </div>
+    
+      <div className='selfie'>
+        <img alt='selfie' src='../../../images/selfie.jpg' />
+      </div>
+      <div className='aboutContent'>
+        <h5>I am a full stack software engineer seeking new opportunities
+        to learn and grow. I operate well within a team setting and lead by example.
+        I welcome new challenges and try to be a part of the great innovation taking
+        place in tech today.
+
+        I am proficient in both front end and back end components of web development
+        including Javascript, jquery, Node.js, Express, HTML, CSS (including frameworks
+        like bootstrap and materialize), front end frameworks like React and Vue,
+        databases like mySQL, MongoDB and more.
+
+        I have a background in hospitality management and have held various management
+        positions at the Kansas City Marriott Downtown. I have managed large scale staffs
+        of 100+ down to tight knit teams of 10. Most recently I have been a food and beverage
+        manager working with a great team and overseeing all bar and restaurant operations
+        within the hotel.
+       </h5>
+      </div>
 
     </div>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,19 @@ import React from 'react';
 function Contact() {
   return (
     <div className="contactPage" id='contact'>
-      
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container contactContent">
+          <h1 className="display-4">Get In Touch</h1>
+          <div className='contentContainer'>
+            <a href='https://github.com/jborgen64' className="lead">GitHub: jborgen64</a>
+            <br />
+            <a href='https://www.linkedin.com/in/jacob-borgen-9793a1124/' className="lead">linkedin: Jacob Borgen</a>
+            <br />
+            <a href='#' className="lead">email: jacobrborgen@gmail.com</a>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
